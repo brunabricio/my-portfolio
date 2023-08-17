25e0bac140e7789697412a35cb0ad4071c10acf4
Bruna Bricio <129636643+brunabricio@users.noreply.github.com>
2023-08-17 18:43:02 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,72 @@
+/*==================== TITLE ANIMATION ====================*/
+function typeWriter (element) {
+  const textoArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textoArray.forEach((letter, i) => {
+    setTimeout(() => element.innerHTML += letter, 75 * i)
+  });
+}
+
+const title = document.querySelector('h1');
+typeWriter(title);
+
 /*==================== ACCORDION SKILLS ====================*/
 
-const skillsContent = document.getElementsByClassName('skills__content'),
-      skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
-      function toggleSkills() {
-        let itemClass = this.parentNode.className;
-    
-        for (i = 0; i < skillsContent.length; i++) {
-            skillsContent[i].className = 'skills__content skills__close';
-        }
-        
-        if (itemClass === 'skills__content skills__close') {
-            this.parentNode.className = 'skills__content skills__open';
-        }
-    }
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
 
 /*==================== OPEN NAV MENU ====================*/
 
 function openNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+/*==================== CARROUSEL ====================*/
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
